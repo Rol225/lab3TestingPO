6f53b26539c2a35d430f990e2b7060e0db246d26
global-arts-1 <testdev@global-arts.ru>
2024-05-30 12:58:03 +0700

    feat(Game): добавлен метод начала игры

--- src/Game.js
+++ src/Game.js
@@ -1,5 +1,8 @@
+import {LampCollection} from "./LampCollection.js";
+
 export class Game {
     _container=null;
+    _lampCollection = new LampCollection();
     _correctSequence = [];
     _currentSequence = [];
     _buttonStart = document.createElement("button");
@@ -9,17 +12,26 @@ export class Game {
         if(!container) throw new Error('Контейнер должен быть либо селектором, либо HTML-элементом');
 
         this.container.innerHTML = '';
+        this.lampCollection.lamps.forEach(lamp => {this.container.appendChild(lamp.el);});
 
         this.buttonStart.type = 'button';
         this.buttonStart.innerHTML = 'Start';
+        this.buttonStart.addEventListener('click', this.startGame.bind(this));
         this.container.appendChild(this.buttonStart);
     }
 
-
+    startGame(){
+        this.lampCollection.deActiveAllLamps();
+        this._currentSequence = [];
+        this._correctSequence = this.lampCollection.getSequence();
+        this.lampCollection.showSequence();
+        this.buttonStart.disabled = true;
+    }
 
     get container(){return this._container;}
     get correctSequence(){return this._correctSequence;}
     get currentSequence(){return this._currentSequence;}
     get gamePlay(){return this._gamePlay;}
     get buttonStart(){return this._buttonStart;}
+    get lampCollection(){return this._lampCollection;}
 }
--- src/tests/Game.test.js
+++ src/tests/Game.test.js
@@ -19,4 +19,12 @@ describe('Game', () => {
     test('constructor', () => {
         expect(game.container).toBeInstanceOf(HTMLElement);
     });
+
+    test('startGame', () => {
+        game.lampCollection.lamps.forEach(lamp => lamp.active());
+        game.startGame();
+        expect(game.currentSequence.length).toBe(0);
+        expect(game.correctSequence.length).toBeGreaterThan(0);
+        game.lampCollection.lamps.forEach(lamp => expect(lamp.state).toBeFalsy());
+    });
 });
